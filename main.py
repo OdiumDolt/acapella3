b3db2cbd7c85f4bca8f4244aacd1a2af56855150
OdiumDolt <rohnheppner@gmail.com>
2024-01-28 10:41:55 -0800

    removed cosign from github actions docker build

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import router
 import commands.music
 import os
+
+
 TOKEN = os.getenv('DISCORD')
 
 # create a Discord client instance
@@ -15,9 +17,12 @@ async def on_message(message: discord.message.Message):
     # check if the message is from the bot or not
     if message.author == client.user:
         return
-    
-    func, args = router.router.find(message.content)
-    await func(args, message)
+    try:
+        func, args = router.router.find(message.content)
+        await func(args, message)
+    except:
+        pass
+        
 
 # start the Discord bot
 client.run(TOKEN)
